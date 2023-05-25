9c99af7d0b9237906f17beee080b1beda434478c
Abhimanue T J <abhimanuetj12@gmail.com>
2023-05-25 15:27:59 +0530

    insatlled react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Home from './pages/Homee';
 import Starred from './pages/Starred';
 import Showw from './pages/Showw';
 import MainLayout from './components/MainLayout';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/Starred" element={<Starred />} />
-        </Route>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/Starred" element={<Starred />} />
+          </Route>
 
-        <Route path="/show/:showId" element={<Showw />} />
+          <Route path="/show/:showId" element={<Showw />} />
 
-        <Route path="*" element={<div>not found</div>} />
-        {/* <Route path="/" element={<App />}>
+          <Route path="*" element={<div>not found</div>} />
+          {/* <Route path="/" element={<App />}>
           <Route index element={<Home />} />
           <Route path="teams" element={<Teams />}>
             <Route path=":teamId" element={<Team />} />
@@ -30,8 +34,9 @@ function App() {
         </Route>
         <Route path="contact-us" element={<Contact />} />
   */}
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
