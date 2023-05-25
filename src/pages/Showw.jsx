d86b448629414882b632b6cc3dcc9983f9aeba5c
Abhimanue T J <abhimanuetj12@gmail.com>
2023-05-25 09:08:25 +0530

    added react hook and custom hooks and put it in function

--- src/pages/Showw.jsx
+++ src/pages/Showw.jsx
@@ -1,7 +1,38 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { getShowById } from '../api/tvmaze';
+
+const useShowById = showId => {
+  const [showData, setShowData] = useState(null);
+  const [showError, setShowError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await getShowById(showId);
+        setShowData(data);
+      } catch (err) {
+        setShowError(err);
+      }
+    }
+
+    fetchData();
+  }, [showId]);
+
+  return { showData, showError };
+};
 
 const Showw = () => {
   const { showId } = useParams();
+  const { showData, showError } = useShowById(showId);
+
+  if (showError) {
+    return <div>We have an error: {showError.message}</div>;
+  }
+
+  if (showData) {
+    return <div>Got show data: {showData.name}</div>;
+  }
 
   return <div>show page for show {showId}</div>;
 };
